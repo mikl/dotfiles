391f7da92a0daa69db81639d277167b79ba4f527
Mikkel Hoegh <mikkel@hoegh.org>
2024-05-15 23:16:39 +0200

    Use next-ls with elixir-tools

--- config/nvim/lua/plugins/elixir-tools.lua
+++ config/nvim/lua/plugins/elixir-tools.lua
@@ -1,24 +1,16 @@
 return {
   "elixir-tools/elixir-tools.nvim",
+  version = "*",
   ft = { "elixir", "eex", "heex", "surface" },
+  event = { "BufReadPre", "BufNewFile" },
   config = function()
     local elixir = require("elixir")
-    local elixirls = require("elixir.elixirls")
 
     elixir.setup({
+      nextls = { enable = true },
       credo = {},
       elixirls = {
-        enabled = true,
-        settings = elixirls.settings({
-          dialyzerEnabled = false,
-          enableTestLenses = false,
-        }),
-        on_attach = function(client, bufnr)
-          -- whatever keybinds you want, see below for more suggestions
-          vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
-          vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
-          vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
-        end,
+        enable = false,
       },
     })
   end,
