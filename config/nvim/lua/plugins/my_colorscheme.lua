1d18c1ec1c62fe96b55b8fac64c7a7580478a3ac
Mikkel Hoegh <mikkel@hoegh.org>
2024-05-16 15:31:59 +0200

    Use the Eldritch color scheme for NeoVim

---
+++ config/nvim/lua/plugins/my_colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "eldritch-theme/eldritch.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "eldritch",
+    },
+  },
+}
