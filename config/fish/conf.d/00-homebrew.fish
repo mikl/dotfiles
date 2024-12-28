b5b00efb4d212d60449e2ddc18733a21db9670d3
Mikkel Hoegh <mikkel@hoegh.org>
2024-12-28 13:14:47 +0100

    Support various Homebrew locations

---
+++ config/fish/conf.d/00-homebrew.fish
@@ -0,0 +1,10 @@
+# search in common homebrew paths (macOS (ARM), Linux (global/default), Linux (alternate), macOS (Intel))
+set -l common_brew_paths /opt/homebrew /home/linuxbrew/.linuxbrew ~/.linuxbrew /usr/local
+for brewpath in $common_brew_paths
+    if test -x "$brewpath/bin/brew"
+        eval ("$brewpath/bin/brew" shellenv)
+        break
+    else
+        echo "nope $brewpath"
+    end
+end
